183cbc1c9acbd52d237669df397aa84795266ee1
Benjamin Pilcher <ben.pilcher@tackmobile.com>
2014-06-03 13:02:36 -0600

    Initial Commit

---
+++ YahooWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YahooWeather
+//
+//  Created by Ben Pilcher on 6/3/14.
+//  Copyright (c) 2014 Pearson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
